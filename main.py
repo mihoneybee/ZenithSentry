6945d4002dbdbb6325854befc887bbbb1353ee10
Mel Isis <honeybee7415@gmail.com>
2026-03-16 15:45:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from src.logic import calcular_status_saude
 
 def executar():
     print("--- 🛡️ ZenithSentry: Monitor de Saúde Mental ---")
-    try:
-        horas = float(input("Digite o total de horas trabalhadas hoje: "))
-        status, mensagem = calcular_status_saude(horas)
-        
-        print(f"\n[{status}]")
-        print(mensagem)
-    except ValueError as e:
-        print(f"Erro: {e}. Por favor, digite um número válido.")
+    while True:
+        try:
+            horas = float(input("Digite o total de horas trabalhadas hoje: "))
+            status, mensagem = calcular_status_saude(horas)
+            
+            print(f"\n[{status}]")
+            print(mensagem)
+            break
+        except ValueError as e:
+            print(f"Erro: {e}. Por favor, digite um número válido.")
+    
 
 if __name__ == "__main__":
     executar()
